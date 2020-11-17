c9ea3ca9692907b5c8ebc71d94f35d6626f7a42a
Forrest Morrisey <60801874+fmorrisey@users.noreply.github.com>
2020-11-17 14:21:23 -0600

    Built Database, rebuilt the customer model into one table, recreated the customer controller without entity

---
+++ TrashCollectorWebApp/Controllers/CustomerController.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TrashCollectorWebApp.Data;
+using TrashCollectorWebApp.Models;
+
+namespace TrashCollectorWebApp.Controllers
+{
+    public class CustomerController : Controller
+    {
+
+        private ApplicationDbContext _dbContext;
+
+        public CustomerController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        // GET: CustomerController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: CustomerController/Details/5
+        public ActionResult Details(int id)
+        {
+            var details = _dbContext.Customers.Find(id);
+            return View();
+        }
+
+        // GET: CustomerController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CustomerController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Customer customer)
+        {
+            try
+            {
+                _dbContext.Customers.Add(customer);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CustomerController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var editCustomer = _dbContext.Customers.Find(id);
+            return View(editCustomer);
+        }
+
+        // POST: CustomerController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, Customer customer)
+        {
+            try
+            {
+                _dbContext.Customers.Update(customer);
+                _dbContext.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        /*
+        // GET: CustomerController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: CustomerController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        */
+    }
+}
